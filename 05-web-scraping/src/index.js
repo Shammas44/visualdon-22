d40f6dbd607822d2b98d6fbe10078f3234fc78eb
Shammas44 <sebastien.traber@hotmail.com>
2022-05-04 19:23:27 +0200

    exercise webscrapping completed

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -1,4 +1,51 @@
 import jsdom from "jsdom";
-import fetch from "isomorphic-fetch"
-import puppeteer from "puppeteer"
+import fetch from "isomorphic-fetch";
+import puppeteer from "puppeteer";
 
+const url1 =
+	"https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales";
+const url2 =
+	"https://www.webscraper.io/test-sites/e-commerce/allinone/computers/laptops";
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url1, { waitUntil: "networkidle2" });
+	await page.waitForSelector(".wikitable");
+	const cantons = await page.$(".wikitable");
+	const cantonsName = await page.$$(`.wikitable  a[title^="Canton"]`);
+	const cantonsList = [];
+	cantonsName.forEach(async (canton) => {
+		const getInnerHTMLProperty = await canton.getProperty("innerHTML");
+		const value = await getInnerHTMLProperty.jsonValue();
+		cantonsList.push(value);
+	});
+	await cantons.screenshot({ path: "cantons.jpg" });
+	await page.close();
+	await browser.close();
+	console.log(cantonsList);
+})();
+
+// ==========================================================================
+// Exercice webscraper.io
+// ==========================================================================
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url2);
+	const price = await page.evaluate(() => {
+		return Array.from(document.querySelectorAll(".thumbnail")).map(
+			(article) => {
+				const price = article.querySelector(".price").innerText;
+				const title = article.querySelector(".title").title;
+				const star = article.querySelector(".ratings > p:nth-child(2)").dataset
+					.rating;
+				return { price, star, title };
+			}
+		);
+	});
+
+	await page.close();
+	await browser.close();
+	console.log(price);
+})();
